1b297678c07db3853df7b3238692233afc1d3c78
dohyeon-han <limited.hyeon@gmail.com>
2021-06-22 23:50:54 +0900

    js animation

--- Reservation_main/animation/src/main/webapp/test.js
+++ Reservation_main/animation/src/main/webapp/test.js
@@ -5,7 +5,7 @@ const interval = window.setInterval(()=>{
 
 //재귀를 이용해 지연 없이 호출 가능
 //c와는 다르게 호출이 끝난 함수는 stack에서 비워짐
-let cnt = 0;
+var cnt = 0;
 function animate(){
 	setTimeout(()=>{
 		if(cnt===10) return;
@@ -19,14 +19,22 @@ animate();
 //settimeout 대신 최적화한 함수 requestAnimationFrame을 이용한 애니메이션
 const box = document.querySelector(".box");
 box.style.left = "0px";
+box.style.transform = "scale(2)";
 
-var cnt = 0;
+var cnt2 = 0;
 function animate() {
-	cnt++;
-	if (cnt % 60 == 0) {
-		box.style.left = parseInt(box.style.left) + cnt + "px";
+	if(cnt2>300) return;
+	if (cnt2 % 60 == 0) {
+		box.style.left = parseInt(box.style.left) + cnt2 + "px";
 	}
+	cnt2++;
 	requestAnimationFrame(animate);
 }
 
-requestAnimationFrame(animate);
+//버튼을 누르면 오른쪽으로 이동
+const btn = document.querySelector("button");
+btn.addEventListener("click", right);
+
+function right(){
+	box.style.left = parseInt(box.style.left) - 50 + "px";
+}
