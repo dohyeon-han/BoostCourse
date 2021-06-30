a582200c792dd6fcf0d0154c401b3221af6776c5
dohyeon-han <limited.hyeon@gmail.com>
2021-06-30 23:46:57 +0900

    DTO

---
+++ Reservation_main/guestbook/src/main/java/kr/or/connect/guestbook/dto/Guestbook.java
@@ -0,0 +1,41 @@
+package kr.or.connect.guestbook.dto;
+
+import java.util.Date;
+
+public class Guestbook {
+	private int id;
+	private String name;
+	private String content;
+	private Date regdate;
+	
+	public Guestbook(){}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	@Override
+	public String toString() {
+		return "Guestbook [id=" + id + ", name=" + name + ", content=" + content + "]";
+	}
+}
