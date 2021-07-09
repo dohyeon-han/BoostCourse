5f303eb1b3a6aed64cfec69aedf44aa18916fef2
dohyeon-han <limited.hyeon@gmail.com>
2021-07-09 20:13:41 +0900

    JS Array method

---
+++ Reservation_detail/arrayMethod.js
@@ -0,0 +1,7 @@
+var data = [{title : "hello",content : "간지철철", price : 12000},
+            {title : "crong",content : "괜춘한 상품", price : 5500},
+            {title : "codesquad",content : "쩌는상품", price : 1200}];
+
+data.forEach(v=>{
+    console.log(v.title,v.content);
+});
