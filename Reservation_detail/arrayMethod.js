70279fbd783161016ead72a2736e7c114c00c7f5
dohyeon-han <limited.hyeon@gmail.com>
2021-07-09 20:48:10 +0900

    Js Attary method

--- Reservation_detail/arrayMethod.js
+++ Reservation_detail/arrayMethod.js
@@ -1,20 +1,29 @@
-var data = [{title : "hello",content : "간지철철", price : 12000},
+var dataObj = {
+        data : [{title : "hello",content : "간지철철", price : 12000},
             {title : "crong",content : "괜춘한 상품", price : 5500},
-            {title : "codesquad",content : "쩌는상품", price : 1200}];
+            {title : "codesquad",content : "쩌는상품", price : 1200}],
+        getData(){
+            console.log(this.data);
+        }
 
-data.forEach(v=>{//for 사용 안해도 됨, 실수 줄임, 짧다
+
+}
+
+dataObj.data.forEach(v=>{//for 사용 안해도 됨, 실수 줄임, 짧다
     console.log(v.title,v.content);
 });
 
-var priceData = data.map(v=>{
+var priceData = dataObj.data.map(v=>{
     return v.price;
 });
 console.table(priceData);
 
-var bigPriceData = data.filter(v=>{//연속해서 사용가능
+var bigPriceData = dataObj.data.filter(v=>{//연속해서 사용가능
     return v.price>5000;
 }).map(v=>{
     var obj = {title:v.title,content:v.content,price:v.price*2};
     return obj;
 })
+
 console.log(bigPriceData);
+dataObj.getData();
